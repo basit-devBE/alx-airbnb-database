53ea5c520125178bdd5f3294436e5a5ebc261415
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-11-28 19:28:10 +0000

    aggregated-sql functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,24 +1,35 @@
 SELECT 
-    user_id,
+    user_id, 
     COUNT(booking_id) AS total_bookings
 FROM 
-    bookings
+    Booking
 GROUP BY 
-    user_id;
+    user_id
+ORDER BY 
+    total_bookings DESC;
 
 
 WITH PropertyBookingCounts AS (
     SELECT 
-        property_id,
-        COUNT(booking_id) AS total_bookings
+        property_id, 
+        COUNT(booking_id) AS booking_count,
+        ROW_NUMBER() OVER (ORDER BY COUNT(booking_id) DESC) AS booking_rank,
+        RANK() OVER (ORDER BY COUNT(booking_id) DESC) AS booking_dense_rank
     FROM 
-        bookings
+        Booking
     GROUP BY 
         property_id
 )
 SELECT 
-    property_id,
-    total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS rank
+    p.property_id,
+    p.name,
+    p.location,
+    pbc.booking_count,
+    pbc.booking_rank,
+    pbc.booking_dense_rank
 FROM 
-    PropertyBookingCounts;
+    PropertyBookingCounts pbc
+JOIN 
+    Property p ON pbc.property_id = p.property_id
+ORDER BY 
+    pbc.booking_count DESC;
