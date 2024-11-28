09d0a04390016747cf3a599bb744f93f62502482
Mohammed Abdul Basit <basitmohammed362@gmail.com>
2024-11-28 19:20:51 +0000

    aggregated-sql functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,15 +1,24 @@
 SELECT 
-    property_id,
-    total_bookings,
-    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_number
-FROM (
+    user_id,
+    COUNT(booking_id) AS total_bookings
+FROM 
+    bookings
+GROUP BY 
+    user_id;
+
+
+WITH PropertyBookingCounts AS (
     SELECT 
         property_id,
-        COUNT(*) AS total_bookings
+        COUNT(booking_id) AS total_bookings
     FROM 
-        Booking
+        bookings
     GROUP BY 
         property_id
-) AS property_totals
-ORDER BY 
-    row_number;
+)
+SELECT 
+    property_id,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS rank
+FROM 
+    PropertyBookingCounts;
